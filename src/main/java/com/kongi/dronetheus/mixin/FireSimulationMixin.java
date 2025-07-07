1db8ad66bd3d78db4e9a00f3947d0df54af7fe20
Leon Ahuja <leon.ahuja@tandm.ai>
2025-07-07 12:54:39 +1000

    base build for duplicating fire function

--- src/main/java/com/kongi/dronetheus/mixin/FireSimulationMixin.java
+++ src/main/java/com/kongi/dronetheus/mixin/FireSimulationMixin.java
@@ -22,92 +22,93 @@ import com.kongi.dronetheus.WindManager;
 
 @Mixin(FireBlock.class)
 public class FireSimulationMixin {
+    //For accessing static properties
     @Shadow @Final public static IntProperty AGE;
 
-    @Inject(method = "scheduledTick", at = @At("HEAD"))
+//    This ensures NOTHING runs after this, even other mods
+    @Inject(method = "scheduledTick", at = @At("HEAD"), cancellable = true)
     private void onFireTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        //For class methods
         FireBlock thisObject = (FireBlock) (Object)this;
+        //For static methods
         FireBlockAccessor accessor = (FireBlockAccessor) thisObject;
-        return;
 
-//
-//        world.scheduleBlockTick(pos, thisObject, FireBlockAccessor.invokeGetFireTickDelay(world.random));
-//
-//
-//        if (world.getGameRules().getBoolean(GameRules.DO_FIRE_TICK)) {
-//            if (!state.canPlaceAt(world, pos)) {
-//                world.removeBlock(pos, false);
-//            }
-//
-//            BlockState blockState = world.getBlockState(pos.down());
-//            boolean bl = blockState.isIn(world.getDimension().infiniburn());
-//            int i = (Integer)state.get(AGE);
-//
-//            accessor.invokeTrySpreadingFire(world, pos.east(), 1 , random, i*3);
-//            if (!bl && world.isRaining() && accessor.invokeIsRainingAround(world, pos) && random.nextFloat() < 0.2F + (float)i * 0.03F) {
-//                world.removeBlock(pos, false);
-//            } else {
-//                int j = Math.min(15, i + random.nextInt(3) / 2);
-//                if (i != j) {
-//                    state = (BlockState)state.with(AGE, j);
-//                    world.setBlockState(pos, state, 4);
-//                }
-//
-//                if (!bl) {
-//                    if (!accessor.invokeAreBlocksAroundFlammable(world, pos)) {
-//                        BlockPos blockPos = pos.down();
-//                        if (!world.getBlockState(blockPos).isSideSolidFullSquare(world, blockPos, Direction.UP) || i > 3) {
-//                            world.removeBlock(pos, false);
-//                        }
-//
-//                        return;
-//                    }
-//
-//                    if (i == 15 && random.nextInt(4) == 0 && !accessor.invokeIsFlammable(world.getBlockState(pos.down()))) {
-//                        world.removeBlock(pos, false);
-//                        return;
-//                    }
-//                }
-//
-//                boolean bl2 = world.getBiome(pos).isIn(BiomeTags.INCREASED_FIRE_BURNOUT);
-//                int k = bl2 ? -50 : 0;
-//                accessor.invokeTrySpreadingFire(world, pos.east(), 1 , random, i*3);
-//                accessor.invokeTrySpreadingFire(world, pos.west(), 300 + k, random, i);
-//                accessor.invokeTrySpreadingFire(world, pos.down(), 250 + k, random, i);
-//                accessor.invokeTrySpreadingFire(world, pos.up(), 250 + k, random, i);
-//                accessor.invokeTrySpreadingFire(world, pos.north(), 300 + k, random, i);
-//                accessor.invokeTrySpreadingFire(world, pos.south(), 300 + k, random, i);
-//                BlockPos.Mutable mutable = new BlockPos.Mutable();
-//
-//                for(int l = -1; l <= 1; ++l) {
-//                    for(int m = -1; m <= 1; ++m) {
-//                        for(int n = -1; n <= 4; ++n) {
-//                            if (l != 0 || n != 0 || m != 0) {
-//                                int o = 100;
-//                                if (n > 1) {
-//                                    o += (n - 1) * 100;
-//                                }
-//
-//                                mutable.set(pos, l, n, m);
-//                                int p = accessor.invokeGetBurnChance(world, mutable);
-//                                if (p > 0) {
-//                                    int q = (p + 40 + world.getDifficulty().getId() * 7) / (i + 30);
-//                                    if (bl2) {
-//                                        q /= 2;
-//                                    }
-//
-//                                    if (q > 0 && random.nextInt(o) <= q && (!world.isRaining() || !accessor.invokeIsRainingAround(world, mutable))) {
-//                                        int r = Math.min(15, i + random.nextInt(5) / 4);
-//                                        world.setBlockState(mutable, accessor.invokeGetStateWithAge(world, mutable, r), 3);
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//
-//            }
-//        }
-//        return;
+        world.scheduleBlockTick(pos, thisObject, FireBlockAccessor.invokeGetFireTickDelay(world.random));
+
+        if (world.getGameRules().getBoolean(GameRules.DO_FIRE_TICK)) {
+            if (!state.canPlaceAt(world, pos)) {
+                world.removeBlock(pos, false);
+            }
+
+            BlockState blockState = world.getBlockState(pos.down());
+            boolean bl = blockState.isIn(world.getDimension().infiniburn());
+            int i = (Integer)state.get(AGE);
+
+            if (!bl && world.isRaining() && accessor.invokeIsRainingAround(world, pos) && random.nextFloat() < 0.2F + (float)i * 0.03F) {
+                world.removeBlock(pos, false);
+            } else {
+                int j = Math.min(15, i + random.nextInt(3) / 2);
+                if (i != j) {
+                    state = (BlockState)state.with(AGE, j);
+                    world.setBlockState(pos, state, 4);
+                }
+
+                if (!bl) {
+                    if (!accessor.invokeAreBlocksAroundFlammable(world, pos)) {
+                        BlockPos blockPos = pos.down();
+                        if (!world.getBlockState(blockPos).isSideSolidFullSquare(world, blockPos, Direction.UP) || i > 3) {
+                            world.removeBlock(pos, false);
+                        }
+
+                        return;
+                    }
+
+                    if (i == 15 && random.nextInt(4) == 0 && !accessor.invokeIsFlammable(world.getBlockState(pos.down()))) {
+                        world.removeBlock(pos, false);
+                        return;
+                    }
+                }
+
+                boolean bl2 = world.getBiome(pos).isIn(BiomeTags.INCREASED_FIRE_BURNOUT);
+                int k = bl2 ? -50 : 0;
+
+                accessor.invokeTrySpreadingFire(world, pos.east(), 300 + k, random, i);
+                accessor.invokeTrySpreadingFire(world, pos.west(), 300 + k, random, i);
+                accessor.invokeTrySpreadingFire(world, pos.down(), 250 + k, random, i);
+                accessor.invokeTrySpreadingFire(world, pos.up(), 250 + k, random, i);
+                accessor.invokeTrySpreadingFire(world, pos.north(), 300 + k, random, i);
+                accessor.invokeTrySpreadingFire(world, pos.south(), 300 + k, random, i);
+                BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+                for(int l = -1; l <= 1; ++l) {
+                    for(int m = -1; m <= 1; ++m) {
+                        for(int n = -1; n <= 4; ++n) {
+                            if (l != 0 || n != 0 || m != 0) {
+                                int o = 100;
+                                if (n > 1) {
+                                    o += (n - 1) * 100;
+                                }
+
+                                mutable.set(pos, l, n, m);
+                                int p = accessor.invokeGetBurnChance(world, mutable);
+                                if (p > 0) {
+                                    int q = (p + 40 + world.getDifficulty().getId() * 7) / (i + 30);
+                                    if (bl2) {
+                                        q /= 2;
+                                    }
+
+                                    if (q > 0 && random.nextInt(o) <= q && (!world.isRaining() || !accessor.invokeIsRainingAround(world, mutable))) {
+                                        int r = Math.min(15, i + random.nextInt(5) / 4);
+                                        world.setBlockState(mutable, accessor.invokeGetStateWithAge(world, mutable, r), 3);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+        ci.cancel();
     }
 }
